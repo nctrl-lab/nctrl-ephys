863aa8c60734c2f99b7d3c7d606b98b86c593980
Dohoung Kim <lapis42@gmail.com>
2024-08-21 23:16:13 +0900

    Added loading function

--- ephys/bmi.py
+++ ephys/bmi.py
@@ -9,7 +9,7 @@ from .spikeglx import read_bin
 
 class BMI:
     def __init__(self, path=None):
-        path = finder(path, pattern='\.prb$', folder=True)
+        path = finder(path, pattern=r'\.prb$', folder=True)
 
         if not path or not os.path.exists(path):
             raise ValueError(f'Path {path} does not exist')
@@ -19,22 +19,13 @@ class BMI:
         self.file_paths = {fn: os.path.join(path, fn) for fn in self.file_names if os.path.exists(os.path.join(path, fn))}
         self.file_paths['prb'] = next((os.path.join(path, fn) for fn in os.listdir(path) if fn.endswith('.prb')), None)
 
-        self.prb_fn = self.file_paths.get('prb')
-        self.mua_fn = self.file_paths.get('mua.bin')
-        self.spk_fn = self.file_paths.get('spk.bin')
-        self.spk_wav_fn = self.file_paths.get('spk_wav.bin')
-        self.fet_fn = self.file_paths.get('fet.bin')
-        self.model_fn = self.file_paths.get(os.path.join('spktag', 'model.pd'))
+        for file_type in ['prb', 'mua.bin', 'spk.bin', 'spk_wav.bin', 'fet.bin', os.path.join('spktag', 'model.pd')]:
+            setattr(self, f"{file_type.split('.')[0]}_fn", self.file_paths.get(file_type))
 
-        print(f'  prb:     {self.prb_fn}\n'
-              f'  mua:     {self.mua_fn}\n'
-              f'  spk:     {self.spk_fn}\n'
-              f'  spk_wav: {self.spk_wav_fn}\n'
-              f'  fet:     {self.fet_fn}\n'
-              f'  model:   {self.model_fn}')
+        print('\n'.join(f"  {k}: {v}" for k, v in self.file_paths.items()))
 
         self.n_channel = 160
-        self.sample_rate = 25000
+        self.sample_rate = 25000.0
         self.binary_radix = 13
         self.uV_per_bit = 0.195
 
@@ -45,8 +36,8 @@ class BMI:
             with open(self.prb_fn, 'r') as f:
                 prb = json.load(f)
                 self.n_channel = prb['params']['n_ch']
-                self.sample_rate = prb['params']['fs']
-                self.channel_id = np.zeros(self.n_channel, dtype=int)
+                self.sample_rate = float(prb['params']['fs'])
+                self.channel_id = np.arange(self.n_channel, dtype=int)
                 self.channel_position = np.full((self.n_channel, 2), np.nan)
                 for i, (key, value) in enumerate(prb['pos'].items()):
                     self.channel_id[i] = int(key)
@@ -57,7 +48,8 @@ class BMI:
         ax.scatter(self.channel_position[:, 0], self.channel_position[:, 1], alpha=0.6, marker='s', s=4)
 
         for i in range(self.n_channel):
-            ax.annotate(self.channel_id[i], (self.channel_position[i, 0], self.channel_position[i, 1]), textcoords='offset points', xytext=(3, 0), ha='left', va='center', fontsize=8)
+            ax.annotate(self.channel_id[i], (self.channel_position[i, 0], self.channel_position[i, 1]), 
+                        textcoords='offset points', xytext=(3, 0), ha='left', va='center', fontsize=8)
         ax.set_xlabel('x position (um)')
         ax.set_ylabel('y position (um)')
         ax.set_title(f'Probe {self.prb_fn}')
@@ -74,10 +66,8 @@ class BMI:
         Voltage Step Size of ADC (Least Significant Bit): 0.195 uV
         """
         selected_channel = self.channel_id[channel_idx]
-        # selected_channel = channel_idx
-        self.binary_radix = 13
-        self.uV_per_bit = 0.195
-        data = read_bin(self.mua_fn, n_channel=self.n_channel, dtype='int32', channel_idx=selected_channel, sample_range=sample_range)
+        data = read_bin(self.mua_fn, n_channel=self.n_channel, dtype='int32', 
+                        channel_idx=selected_channel, sample_range=sample_range)
 
         return data / (2 ** self.binary_radix) * self.uV_per_bit
     
@@ -87,16 +77,55 @@ class BMI:
         data -= np.median(data, axis=0, keepdims=True)
         data -= np.median(data, axis=1, keepdims=True)
 
+        plt.figure(figsize=(12, 8))
         for i in range(data.shape[1]):
-            plt.plot(data[:, i] + i * 100, label=f'Channel {i}', color='k')  # Offset each channel for visibility
+            plt.plot(data[:, i] + i * 100, color='k', linewidth=0.5)
         plt.xlabel('Samples')
         plt.ylabel('Channel')
         plt.title('Raw data')
         plt.show()
 
+    def load_spk(self):
+        if self.spk_fn:
+            spk = np.fromfile(self.spk_fn, dtype='<i4').reshape(-1, 2)
+            self.spk_frame = spk[:, 0]
+            self.spk_channel = spk[:, 1]  # Note: not in physical order
+    
+    def load_spk_wav(self):
+        if self.spk_wav_fn:
+            spk_wav = np.fromfile(self.spk_wav_fn, dtype=np.int32).reshape(-1, 20, 4)
+            self.spk_peak_ch, self.spk_time, self.electrode_group = spk_wav[..., 0, 1], spk_wav[..., 0, 2], spk_wav[..., 0, 3]
+            self.spk_wav = spk_wav[..., 1:, :] / (2**self.binary_radix) * self.uV_per_bit
+    
+    def load_fet(self):
+        if self.fet_fn:
+            scale_factor = float(2**self.binary_radix) / self.uV_per_bit
+
+            fet = np.fromfile(self.fet_fn, dtype=np.int32).reshape(-1, 8)
+            self.fet = pd.DataFrame({
+                'frame': fet[:, 0].astype(np.int64),
+                'group_id': fet[:, 1],
+                'fet0': fet[:, 2].astype(np.float32) / scale_factor,
+                'fet1': fet[:, 3].astype(np.float32) / scale_factor,
+                'fet2': fet[:, 4].astype(np.float32) / scale_factor,
+                'fet3': fet[:, 5].astype(np.float32) / scale_factor,
+                'spike_id': fet[:, 6],
+                'spike_energy': fet[:, 7].astype(np.float32) / scale_factor
+            })
+
+            # Check for any potential issues in the loaded data
+            if self.fet['frame'].diff().min() < 0:
+                print("\033[91mWarning: Time values are not monotonically increasing.\033[0m")
+            if np.any(np.isnan(self.fet.values)):
+                print("\033[91mWarning: NaN values detected in fet data.\033[0m")
+            if np.any(np.isinf(self.fet.values)):
+                print("\033[91mWarning: Infinite values detected in fet data.\033[0m")
+
+            # TODO: If frame rolls over, add 2^32 to the frame
+
 
 if __name__ == '__main__':
     bmi = BMI('C:\\SGL_DATA')
-    bmi.plot_mua()
-
-
+    bmi.load_spk()
+    bmi.load_spk_wav()
+    bmi.load_fet()
